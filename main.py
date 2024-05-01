a25ce7f7c60ca0c18cdaa69789a7707188099908
aicorein <melodyecho@glowmem.com>
2024-05-01 18:26:47 +0800

    Update

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 
 sys.path.append("../src/")
+from plugins.env import BOT_INFO
+
 from melobot import *
 
 if __name__ == "__main__":
     bot = MeloBot("MeloInf")
-    conn = ReverseWsConn("127.0.0.1", 9090, reconnect=True)
-    bot.init(conn, log_to_dir="./logs", log_level="DEBUG")
+    conn = ReverseWsConn(
+        "127.0.0.1", 9088, reconnect=True, access_token=BOT_INFO.access_token
+    )
+    bot.init(conn, log_to_dir="./logs", log_level="INFO")
     bot.load_plugins("plugins")
     bot.run()
